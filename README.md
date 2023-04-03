8aaf614796c5b4f5838061927a651476053a37b9
Hedreon <hedreon@e.email>
2023-04-03 23:01:32 +0100

    v1.0.2
    I will go mad if GitHub actually deletes gradle-wrapper.jar, just because of their falsified thought.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Fast Portals
-A Minecraft fabric mod that halves the time it takes to enter or exit the nether via portal. The mod is required to be installed on servers, or can be installed on the client for singleplayer use.
+
+A Fabric mod that lessens the time it takes to enter or exit nether portals.
+
+The mod is required to be installed on servers, or can be installed on the client for single-player use.
--- src/main/java/com/glue_gunner4/fastportals/mixin/FastPortalsMixin.java
+++ src/main/java/com/glue_gunner4/fastportals/mixin/FastPortalsMixin.java
@@ -2,18 +2,15 @@ package com.glue_gunner4.fastportals.mixin;
 
 import net.minecraft.entity.player.PlayerAbilities;
 import net.minecraft.entity.player.PlayerEntity;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PlayerEntity.class)
 public class FastPortalsMixin {
-    @Shadow
-    private PlayerAbilities abilities;
-    @Inject(method = "getMaxNetherPortalTime", at = @At("HEAD"), cancellable = true)
-    public void getMaxNetherPortalTime(CallbackInfoReturnable<Integer> cir){
-        cir.setReturnValue(abilities.invulnerable ? 1 : 40);
+    @Final
+    private final PlayerAbilities abilities = new PlayerAbilities();
+
+    public int getMaxNetherPortalTime() {
+        return this.abilities.invulnerable ? 1 : 40;
     }
 }
